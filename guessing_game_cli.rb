dd742489ab808d0aaf040139fa0b119f36dbf745
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-11 19:45:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   user_guess = gets.chomp
   if user_guess == "exit"
     puts "You guessed the correct number"
-    elsif user_guess == "exit"
+  
     puts "Goodbye!"
       elsif user_guess.to_i != guess
         puts "Sorry! The computer guessed #{guess}"
