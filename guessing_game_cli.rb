cf255262c2c7f431e0e6ff706aed5928ef027dac
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-11 19:30:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,9 @@ require 'pry'
 def run_guessing_game
   guess = rand(6) + 1
   puts "Please guess a number between 1 and 6:"
-  
+  user_guess = gets.chomp
+  if user_guess == "exit"
+    puts "Goodbye!"
+  end
+    
 end
