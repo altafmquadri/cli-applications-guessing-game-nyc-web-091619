7792ce34abf4494d96d4165091f380a61f6e4042
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-11 19:40:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,6 @@ def run_guessing_game
   guess = rand(6) + 1
   puts "Please guess a number between 1 and 6:"
   user_guess = gets.chomp
-  if user_guess == "exit"
-    puts "Goodbye!"
-  end
   if user_guess.to_i == guess
     puts "You guessed the correct number!"
       elsif user_guess.to_i != guess
